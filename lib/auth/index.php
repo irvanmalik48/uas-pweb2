d039e779ee62e619e0f5c9a2488953372eea1181
Irvan Malik <irvanmalik48@gmail.com>
2021-11-07 22:42:23 +0700

    feat: implement checking and session flashcard
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- lib/auth/index.php
+++ lib/auth/index.php
@@ -23,9 +23,26 @@ if (isset($_POST["login"])) {
         if (password_verify($pass, $user["pass"])) {
             session_start();
             $_SESSION["user"] = $user;
+            if (empty($_SESSION["token"])) {
+                if (function_exists("random_bytes")) {
+                    $_SESSION["token"] = bin2hex(random_bytes(32));
+                } else {
+                    $_SESSION["token"] = bin2hex(
+                        openssl_random_pseudo_bytes(32)
+                    );
+                }
+            }
             header("Location: ../../");
         } else {
+            session_start();
+            $_SESSION["error"] = "Username atau password salah.";
+
             header("Location: ../../pages/login/");
         }
+    } else {
+        session_start();
+        $_SESSION["error"] = "Username atau password salah.";
+
+        header("Location: ../../pages/login/");
     }
 }
