cb33721550c17e38347ea7c9c8c3225d35fe196c
Irvan Malik <irvanmalik48@gmail.com>
2021-11-07 22:44:03 +0700

    feat: implement error checking
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- pages/register/index.php
+++ pages/register/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start(); ?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -19,25 +22,30 @@
                 </h5>
                 <form action="../../lib/register/index.php" method="post" enctype="multipart/form-data">
                     <div class="mb-3">
-                        <label for="name" class="form-label h6">Nama Lengkap</label>
+                        <label for="name" class="form-label text-white h6">Nama Lengkap</label>
                         <input type="text" class="form-control" id="name" name="name" aria-describedby="nameSection" required/>
                         <div class="form-text" id="nameSection">Isilah dengan nama lengkap anda.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="uname" class="form-label h6">Username</label>
+                        <label for="uname" class="form-label text-white h6">Username</label>
                         <input type="text" class="form-control" id="uname" name="uname" aria-describedby="unameSection" required/>
                         <div class="form-text" id="unameSection">Isilah dengan username anda.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label h6">Email</label>
+                        <label for="email" class="form-label text-white h6">Email</label>
                         <input type="text" class="form-control" id="email" name="email" aria-describedby="emailSection" required/>
                         <div class="form-text" id="emailSection">Isilah dengan email anda.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="pass" class="form-label h6">Password</label>
+                        <label for="pass" class="form-label text-white h6">Password</label>
                         <input type="password" class="form-control" id="pass" name="pass" aria-describedby="passSection" required/>
                         <div class="form-text" id="passSection">Isilah dengan password pilihan anda.</div>
                     </div>
+                    <?php if (!empty($_SESSION["error"])) { ?>
+                    <p class="text-center bg-nord-accent-red-nohover text-white px-1 py-2">
+                        <?= $_SESSION["error"] ?>
+                    </p>
+                    <?php unset($_SESSION["error"]);} ?>
                     <div class="container-fluid p-0">
                         <button type="submit" name="register" class="btn btn-light bg-nord-accent float-end">Register</button>
                     </div>
@@ -48,3 +56,6 @@
     </div>
 </body>
 </html>
+
+<?php session_destroy();
+?>
