20ef160e7a91fd275a2377a9bc68f2f5018f09e1
Irvan Malik <irvanmalik48@gmail.com>
2021-12-08 16:01:21 +0700

    feat: add `Register` controller
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ app/Controllers/Register.php
@@ -0,0 +1,42 @@
+<?php namespace App\Controllers;
+
+use App\Models\Users;
+ 
+class Register extends BaseController
+{
+    public function index()
+    {
+        helper(['form']);
+        $data = [];
+        echo view('register', $data);
+    }
+ 
+    public function save()
+    {
+        helper(['form']);
+        $rules = [
+            'uname' => 'required|min_length[3]|max_length[20]|is_unique[users.uname]',
+            'name' => 'required|min_length[3]|max_length[100]',
+            'email' => 'required|min_length[6]|max_length[100]|valid_email|is_unique[users.email]',
+            'password' => 'required|min_length[8]|max_length[200]',
+            'confpassword' => 'matches[password]'
+        ];
+         
+        if($this->validate($rules)){
+            $model = new Users();
+            $data = [
+                'uname' => $this->request->getVar('uname'),
+                'name' => $this->request->getVar('name'),
+                'email' => $this->request->getVar('email'),
+                'pass' => password_hash($this->request->getVar('pass'), PASSWORD_DEFAULT)
+            ];
+            $model->save($data);
+            return redirect()->to('/login');
+        }else{
+            $data['validation'] = $this->validator;
+            echo view('register', $data);
+        }
+         
+    }
+ 
+}
