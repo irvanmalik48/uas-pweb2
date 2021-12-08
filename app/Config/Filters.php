3d06e753f821ca49c2ac84860cbb6e9d99ce355e
Irvan Malik <irvanmalik48@gmail.com>
2021-12-08 16:51:43 +0700

    feat: update filter config
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -19,6 +19,7 @@ class Filters extends BaseConfig
         'csrf'     => CSRF::class,
         'toolbar'  => DebugToolbar::class,
         'honeypot' => Honeypot::class,
+        'auth'     => App\Filters\Auth::class
     ];
 
     /**
