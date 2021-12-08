1ab3ec3c29264eb316f94aed5d107769281d5f76
Irvan Malik <irvanmalik48@gmail.com>
2021-12-08 16:50:44 +0700

    feat: add an `Auth` filter that runs before entering pages
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ app/Filters/Auth.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Filters;
+ 
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+ 
+class Auth implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null) {
+        if (!(session()->has('logged_in') && (session()->get('logged_in') === TRUE))) {
+            return redirect()->to('/login');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+        // Intentionally blank
+    }
+}
