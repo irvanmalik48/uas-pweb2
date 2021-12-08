3adaeaf3a34cc9cf378feaf42353ca3f6d9978f7
Irvan Malik <irvanmalik48@gmail.com>
2021-12-08 17:04:33 +0700

    feat: add more routes
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,7 +17,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Dashboard');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -31,7 +31,11 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Dashboard::index', ['filter' => 'auth']);
+$routes->get('/dashboard', 'Dashboard::index', ['filter' => 'auth']);
+$routes->get('/login', 'Login::index', ['filter' => 'logged_in']);
+$routes->get('/register', 'Register::index', ['filter' => 'logged_in']);
+$routes->get('/edit', 'Edit::index', ['filter' => 'auth']);
 
 /*
  * --------------------------------------------------------------------
