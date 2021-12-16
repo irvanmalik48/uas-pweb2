16ca0d30bd58105d4cd39d32a5e965c3e56b1d86
Irvan Malik <irvanmalik48@gmail.com>
2021-12-16 19:53:26 +0700

    feat: initialize views
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ app/Views/dashboard.php
@@ -0,0 +1,63 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <script defer src="assets/js/bootstrap.bundle.min.js"></script>
+    <title>My Profile</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body class="d-flex justify-content-center align-items-center">
+    <div class="parallax"></div>
+    <main class="container container-uh-huh">
+        <div class="row g-4 justify-content-center align-items-start my-lg-5 py-lg-5 my-2 py-2">
+            <div class="d-flex justify-content-center align-items-center col-lg-3 col-12">
+                <img src="<?= $image ?>" class="img shadow">
+            </div>
+            <div class="d-flex col-lg-9 col-12">
+                <div class="flex-fill card shadow c-b">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?= $name ?>
+                        </h5>
+                        <h6 class="card-subtitle mb-2 text-nord-muted">
+                            <?= $email .
+                                " | @" .
+                                $uname ?>
+                        </h6>
+                        <p class="text-nord my-0 p-0">
+                            <strong>NIM</strong>
+                        </p>
+                        <p class="mt-0 p-0">
+                            <?= $nim ?>
+                        </p>
+                        <p class="text-nord my-0 p-0">
+                            <strong>FAKULTAS</strong>
+                        </p>
+                        <p class="mt-0 p-0">
+                            <?= $faculty ?>
+                        </p>
+                        <p class="text-nord my-0 p-0">
+                            <strong>PRODI</strong>
+                        </p>
+                        <p class="mt-0 p-0">
+                            <?= $major ?>
+                        </p>
+                        <p class="text-nord my-0 p-0">
+                            <strong>DESKRIPSI</strong>
+                        </p>
+                        <p class="mt-0 p-0 mb-0">
+                            <?= $description ?>
+                        </p>
+                    </div>
+                    <div class="container-fluid px-0 py-0">
+                        <a href="pages/edit/" class="btn btn-light bg-nord-accent float-start m-4 mt-0">Edit Profil</a>
+                        <a href="pages/logout/" class="btn btn-light bg-nord-accent-red float-end m-4 mt-0">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ app/Views/login.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script defer src="assets/js/bootstrap.bundle.min.js"></script>
+    <title>Login</title>
+</head>
+<body>
+    <div class="parallax"></div>
+    <div class="container container-lr pt-5 pb-5">
+        <div class="py-5"></div>
+        <div class="card c-b mt-5 shadow">
+            <div class="card-body">
+                <h5 class="card-title text-center">
+                    Login
+                </h5>
+                <form action="login/auth" method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="uname" class="form-label text-white h6">Username/Email</label>
+                        <input type="text" class="form-control" id="uname" name="uname" aria-describedby="unameSection" required/>
+                        <div class="form-text" id="unameSection">Isilah dengan username atau email anda.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="pass" class="form-label text-white h6">Password</label>
+                        <input type="password" class="form-control" id="pass" name="pass" aria-describedby="passSection" required/>
+                        <div class="form-text" id="passSection">Isilah dengan password anda.</div>
+                    </div>
+                    <?php if (!empty($msg)) { ?>
+                    <p class="text-center bg-nord-accent-red-nohover text-white px-1 py-2">
+                        <?= $msg ?>
+                    </p>
+                    <?php } ?>
+                    <div class="container-fluid p-0">
+                        <button type="submit" name="login" class="btn btn-light bg-nord-accent float-end">Login</button>
+                    </div>
+                    <a href="../register/" class="btn btn-light bg-nord-accent float-start">Belum punya akun?</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
