8fc92da16deb110e32ded0777d4f50e1fb6b8c0f
Irvan Malik <irvanmalik48@gmail.com>
2021-12-17 01:31:18 +0700

    feat: update `Login` controller
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -11,45 +11,68 @@ class Login extends BaseController
         $this->Users = new Users();
     }
 
-    public function index()
+    public function index(): string
     {
         helper(["form"]);
-        echo view("login");
+        return view("login");
     }
 
-    public function auth(): RedirectResponse
+    public function auth()
     {
         $session = session();
         $uname = $this->request->getPost("uname");
         $password = $this->request->getPost("pass");
         $data = $this->Users->where("uname", $uname)->first();
 
-        if (!$data) {
-            $data = $this->Users->where("email", $uname)->first();
-        }
-
-        if (!$data) {
-            $session->setFlashdata("msg", "Email/Username not found");
-            return redirect()->to("/login");
-        }
-        
-        $pass = $data["pass"];
-        $verify_pass = password_verify($password, $pass);
-
-        if (!$verify_pass) {
-            $session->setFlashdata("msg", "Wrong Password");
-            return redirect()->to("/login");
-        }
-
-        $ses_data = [
-            "user_id" => $data["id"],
-            "user_uname" => $data["uname"],
-            "user_email" => $data["email"],
-            "logged_in" => true,
+        $rules = [
+            "uname" => "required",
+            "pass" => "required",
         ];
 
-        $session->set($ses_data);
+        $messages = [
+            "uname" => [
+                "required" => "Please fill your username/email.",
+            ],
+            "pass" => [
+                "required" => "Please fill your password.",
+            ],
+        ];
 
-        return redirect()->to("/");
+        if (!$this->validate($rules, $messages)) {
+            $session->setFlashdata("msg", $this->validator->getErrors());
+            return redirect()->to("/login");
+        } else {
+            if (!$data) {
+                $data = $this->Users->where("email", $uname)->first();
+            }
+
+            if (!$data) {
+                $session->setFlashdata("msg", [
+                    "exception" => "User not found.",
+                ]);
+                return redirect()->to("/login");
+            }
+
+            $pass = $data["pass"];
+            $verify_pass = password_verify($password, $pass);
+
+            if (!$verify_pass) {
+                $session->setFlashdata("msg", [
+                    "exception" => "Wrong password.",
+                ]);
+                return redirect()->to("/login");
+            }
+
+            $ses_data = [
+                "user_id" => $data["id"],
+                "user_uname" => $data["uname"],
+                "user_email" => $data["email"],
+                "logged_in" => true,
+            ];
+
+            $session->set($ses_data);
+
+            return redirect()->to("/");
+        }
     }
 }
