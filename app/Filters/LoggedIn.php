82d652a31b97b68c9e0b9e102ce51276c58a60db
Irvan Malik <irvanmalik48@gmail.com>
2021-12-17 01:32:28 +0700

    feat: update `LoggedIn` filter
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- app/Filters/LoggedIn.php
+++ app/Filters/LoggedIn.php
@@ -1,20 +1,28 @@
-<?php 
+<?php
 
 namespace App\Filters;
- 
+
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
- 
+
 class LoggedIn implements FilterInterface
 {
-    public function before(RequestInterface $request, $arguments = null) {
-        if (session()->has('logged_in')) {
-            return redirect()->to('/');
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (
+            session()->has("logged_in") &&
+            session()->get("logged_in") === true
+        ) {
+            return redirect()->to("/");
         }
     }
 
-    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+    public function after(
+        RequestInterface $request,
+        ResponseInterface $response,
+        $arguments = null
+    ) {
         // Intentionally blank
     }
 }
