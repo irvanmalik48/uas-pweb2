8a409391c31cc0c40efc489efabe9eec99ab6951
Irvan Malik <irvanmalik48@gmail.com>
2021-12-17 01:31:59 +0700

    feat: update `Auth` filter
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- app/Filters/Auth.php
+++ app/Filters/Auth.php
@@ -1,20 +1,28 @@
-<?php 
+<?php
 
 namespace App\Filters;
- 
+
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
- 
+
 class Auth implements FilterInterface
 {
-    public function before(RequestInterface $request, $arguments = null) {
-        if (!session()->has('logged_in')) {
-            return redirect()->to('/login');
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if (
+            !session()->has("logged_in") &&
+            session()->get("logged_in") !== true
+        ) {
+            return redirect()->to("/login");
         }
     }
 
-    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {
+    public function after(
+        RequestInterface $request,
+        ResponseInterface $response,
+        $arguments = null
+    ) {
         // Intentionally blank
     }
 }
